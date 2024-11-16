b6fef92c8849829fa848651a0535f137d8d4eb74
malidubingusar <slsudhuwa@gmail.com>
2024-11-16 22:12:59 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "ANJU-MD=HIVQ2Job#_G57QJjKT9lTx7QinNcFRErRzqY2-_kt8kMxKIVV9lo",
+SESSION_ID: process.env.SESSION_ID || ANJU-MD=HIVQ2Job#_G57QJjKT9lTx7QinNcFRErRzqY2-_kt8kMxKIVV9lo
 };
